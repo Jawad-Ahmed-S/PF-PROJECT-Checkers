d2f90d81db682e7dc8489a9c02892854477faa01
Cancer45 <youshaafaisal45@gmail.com>
2024-11-27 19:54:04 +0500

    Initial commit

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 //tracks current turn
 char curr_turn = 'W'; //'W' or 'B'
+int turn_move = 1; //used to change values of movement according to curr_turn 
+char gameBoard[8][8];
 
 //position struct
 struct position
@@ -24,8 +26,8 @@ struct piece
 //prints array each turn
 void printArray(char[8][8]);
 
-//sets initial positions
-void initialPositions(char[8][8]);
+//sets initial positions and piece colors
+void intializeValues(char[8][8]);
 
 //updates array after each move
 void updateArray(char[8][8]);
@@ -47,11 +49,10 @@ void changeTurn ();
 
 int main()
 {
-    char gameBoard[8][8];
-    
-    initialPositions(gameBoard);
+    intializeValues(gameBoard);
+    updateArray(gameBoard);
     printArray(gameBoard);
-
+    
     while (true)
     {
         //win checking function called here (must be first so can identify if current_turn cannot move and hence, loses)
@@ -78,15 +79,27 @@ void movement()
         //repeat until selection valid   
     } while (piece == -1);
     
-    do
+    while (true)
     {
         printf("move: ");
         scanf(" %c", &input_move);
+
+        if ((input_move == 'z' || input_move == 'c') && (!pieces[piece].king))
+        {
+            printf("piece not King :| Try Again\n");
+            continue;
+        }
+
+        //get movement values by translateMove
         move_vals = translateMove(input_move);
         what_move = moveValid(piece, move_vals);
-
+        
         //repeat until move valid
-    } while (what_move == -1 || ((input_move == 'z' || input_move == 'c') && (!pieces[piece].king)));
+        if (what_move != -1)
+        break;
+    }
+
+    printf("%d\n", what_move);
 
     //move piece
     if (what_move == -2)
@@ -97,7 +110,7 @@ void movement()
         //check if king
         if ((pieces[piece].position.y == 7 && pieces[piece].color == 'W') || (pieces[piece].position.y == 0 && pieces[piece].color == 'B'))
         {
-            //king is piece from one side is on the other side
+            //king is when piece from one side is on the other side
             pieces[piece].king = true;
         }
     }
@@ -105,18 +118,12 @@ void movement()
     //capture piece
     else
     {
-        while (what_move >= 0)
-        {
-            pieces[what_move].captured = true;
+        //capture piece
+        pieces[what_move].captured = true;
 
-            pieces[piece].position.x += move_vals.x * 2;
-            pieces[piece].position.y += move_vals.y * 2;
-
-            what_move = moveValid(piece, move_vals);
-            //code left for checking both sides for capture and prompt if both capture for both
-        }
-        
-        
+        //change position of selected piece (jump)
+        pieces[piece].position.x += (move_vals.x * 2);
+        pieces[piece].position.y += (move_vals.y * 2);
     }
 
     changeTurn();
@@ -133,6 +140,8 @@ void changeTurn ()
     {
         curr_turn = 'W';
     }
+
+    turn_move *= -1;
 }
 
 int selectionValid (struct position selection) //returns -1 if invalid, index of selected if valid
@@ -153,6 +162,22 @@ int selectionValid (struct position selection) //returns -1 if invalid, index of
             //true if position of piece same as selected
             if (pieces[i].position.x == selection.x && pieces[i].position.y == selection.y)
             {
+                if (moveValid(i, translateMove('a')) == -1 && moveValid(i, translateMove('d')) == -1)
+                {
+                    if (pieces[i].king)
+                    {
+                        if (moveValid(i, translateMove('z')) == -1 && moveValid(i, translateMove('c')) == -1)
+                        {
+                            return -1;
+                        }
+                    }
+
+                    else
+                    {
+                        return -1;
+                    }
+                }
+
                 return i;
             }
         }
@@ -162,17 +187,17 @@ int selectionValid (struct position selection) //returns -1 if invalid, index of
 }
 
 //takes index of selected piece and its change in position
-int moveValid(int index, struct position move) //returns 0 if invalid, 1 if move, index of piece to be captured if capture
+int moveValid(int index, struct position move) //returns -1 if invalid, -2 if move, index of piece to be captured if capture
 {
     struct position tmp;
-    int maybe_capture;
+    int maybe_capture = -1;
 
     tmp.x = pieces[index].position.x + move.x;
     tmp.y = pieces[index].position.y + move.y;
 
     //check if out of bounds
-    if (tmp.x > 7 || tmp.y > 7)
-    {
+    if (tmp.x > 7 || tmp.x < 0 || tmp.y > 7 || tmp.y < 0)
+    {   
         return -1;
     }
 
@@ -187,46 +212,52 @@ int moveValid(int index, struct position move) //returns 0 if invalid, 1 if move
             continue;
         }
 
-        if (pieces[i].color == curr_turn)
+        if (pieces[i].position.x == tmp.x && pieces[i].position.y == tmp.y)
         {
-            if (pieces[i].position.x == tmp.x || pieces[i].position.y == tmp.y)
+            if (pieces[i].color != curr_turn)
             {
                 maybe_capture = i;
-                occupied  = true;
-                break;
-            } 
-        }
+            }
+
+            occupied  = true;
+            break;
+        } 
     }
 
     //check if capture
     if (occupied)
     {
-        tmp.x += move.x;
-        tmp.y += move.y;
-
-
-        //checks if square to jump to also occupied
-        for (int i = 0; i < 24; i++)
+        if (maybe_capture > -1)
         {
-            //if piece captured, cannot occupy square, move on
-            if (pieces[i].captured)
+            tmp.x += move.x;
+            tmp.y += move.y;
+
+
+            //checks if square to jump to also occupied
+            for (int i = 0; i < 24; i++)
             {
-                continue;
-            }
-            
-            //check only pieces that belong to current turn
-            if (pieces[i].color == curr_turn)
-            {
-                if (pieces[i].position.x == tmp.x || pieces[i].position.y == tmp.y)
+                //if piece captured, cannot occupy square, move on
+                if (pieces[i].captured)
                 {
-                    //collision with piece, cannot capture
-                    return -1;
-                    break;
-                } 
+                    continue;
+                }
+
+                //check only pieces that belong to current turn
+                if (pieces[i].color == curr_turn)
+                {
+                    if (pieces[i].position.x == tmp.x && pieces[i].position.y == tmp.y)
+                    {
+                        //collision with piece, cannot capture
+                        return -1;
+                    } 
+                }
             }
+            //if no collision, can capture
+            return maybe_capture;
         }
-        //if no collision, can capture
-        return maybe_capture;
+
+        else
+        return -1;
     }
     //if not occupied, can move
     return -2;
@@ -276,27 +307,43 @@ void updateArray(char gameBoard[8][8])
 
     for (int i = 0; i < 24; i++)
     {
+        if (!pieces[i].captured)
         gameBoard[pieces[i].position.y][pieces[i].position.x] = pieces[i].color; //x is rows y is columns
     }
 }
 
 void printArray(char gameBoard[8][8])
 {
-    for (int i = 0; i < 8; i++)
-    {
+    for (int i = -1; i < 8; i++)
+    {   
+        if (i >= 0)
+        printf("%d ", i);
+        else
+        printf("  ");
+
         for (int j = 0; j < 8; j++)
         {
+            if (i == -1)
+            {
+                printf(" %d ", j);
+                continue;
+            }
+
             printf(" %c ", gameBoard[i][j]);
         }
         printf("\n");
     }
+
+    //which turn?
+    printf("%c playing\n", curr_turn);
 }
-void initialPositions(char gameboard[8][8])
+
+void intializeValues(char gameboard[8][8])
 {
     char initialBoard[8][8] = {
-        {'B', '-', 'B', '-', 'B', '-', 'B', '-'},
         {'-', 'B', '-', 'B', '-', 'B', '-', 'B'},
         {'B', '-', 'B', '-', 'B', '-', 'B', '-'},
+        {'-', 'B', '-', 'B', '-', 'B', '-', 'B'},
         {'-', '-', '-', '-', '-', '-', '-', '-'},
         {'-', '-', '-', '-', '-', '-', '-', '-'},
         {'W', '-', 'W', '-', 'W', '-', 'W', '-'},
@@ -304,11 +351,25 @@ void initialPositions(char gameboard[8][8])
         {'W', '-', 'W', '-', 'W', '-', 'W', '-'},
     };
 
+    int piece_counter = 0;
+
+    //set initial positions and colors
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
         {
-            gameboard[i][j] = initialBoard[i][j];
+            if (initialBoard[i][j] != '-')
+            {
+                //assign color
+                pieces[piece_counter].color = initialBoard[i][j];
+
+                //assign positions
+                pieces[piece_counter].position.x = j;
+                pieces[piece_counter].position.y = i;
+
+                //update counter
+                piece_counter++;
+            }
         }
     }
 }
